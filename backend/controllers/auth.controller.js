2cd4ff1cc74c41cbeffc5dc879297c7b1fa21913
Himanshu Dhammeriya <cobrakai3005@gmail.com>
2026-06-17 11:21:56 +0530

    adding cookie config agin

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -172,7 +172,7 @@ export async function loginAdmin(req, res) {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "lax" : "none",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
